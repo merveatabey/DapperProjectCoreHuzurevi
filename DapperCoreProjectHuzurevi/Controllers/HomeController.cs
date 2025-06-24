c91cc7a166f064403e184ee0f2dc199b0f37ee1f
1-03 <a1-03@Derslik1-03-iMac.local>
2025-06-24 15:42:16 +0300

    first commit

---
+++ DapperCoreProjectHuzurevi/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DapperCoreProjectHuzurevi.Models;
+
+namespace DapperCoreProjectHuzurevi.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+   
+}
+
